dfe124d2391513908ab16c509f6421983ef570b2
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-29 15:20:15 +0100

    completata milestone 2

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,9 @@
-let posts = require('../data/postsContent.js');
+// let posts = require('../data/postsContent.js');
 
 const connection = require('../data/db.js');
 
 function index(req, res) {
-    const sql = 'SELECT * FROM pizzas';
+    const sql = 'SELECT * FROM posts';
 
     connection.query(sql, (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
