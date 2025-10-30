30cf22cc4324468de957a609e9ba99b27598cca0
MatteoCiardi <ciardimatteo99@gmail.com>
2025-10-30 16:01:43 +0100

    aggiunto file .env

--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Toscanoantico1$',
-    database: 'db_blog'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 connection.connect((err) => {
